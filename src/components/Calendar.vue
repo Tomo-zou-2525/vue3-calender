2222c33229b69c142fd2ef10dc7d3b8ad02a0790
友造-2525 <bathbook1990tomozou@gmail.com>
2021-04-15 08:44:40 +0900

    youbiNum に現在の曜日を数字で取得

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -3,18 +3,33 @@
 </template>
 
 <script>
-import moment from "moment"
+import moment from "moment";
 
 export default {
+  data() {
+    return {
+      currentDate: moment(),
+    };
+  },
+  methods: {
+    getStartDate() {
+      let date = moment(this.currentDate);
+      date.startOf("moment");
+      const youbiNum = date.day();
+      return date.subtract(youbiNum, "days");
+    },
+  },
+  mounted() {
+    console.log(this.getStartDate());
+  },
   //インスタンスが生成されてからマウント
-  mounted(){
-    //その月の最初に日を取得
-    let date = moment().startOf("month")
-    //日時を変数に代入
-    const youbiNum = date.day()
-    //ログで確認
-    console.log(youbiNum)
-  }
-}
-
+  // mounted() {
+  //   //その月の最初に日を取得
+  //   let date = moment().startOf("month");
+  //   //日時を変数に代入
+  //   const youbiNum = date.day();
+  //   //ログで確認(日曜日が0)
+  //   console.log(youbiNum);
+  // },
+};
 </script>
