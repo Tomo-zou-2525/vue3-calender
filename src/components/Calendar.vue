41d2745e110acc550e1df35e1d07df004a065a91
友造-2525 <bathbook1990tomozou@gmail.com>
2021-04-14 23:54:51 +0900

    Calendar.vue script記述

---
+++ src/components/Calendar.vue
@@ -0,0 +1,15 @@
+<template>
+  <Calendar></Calendar>
+</template>
+
+<script>
+import Calendar from "./componetns/Calendar.vue"
+
+export default {
+  name: "APP",
+  components: {
+    Calendar
+  }
+}
+
+</script>
